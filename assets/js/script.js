9163af8c88ed3e3d3c8efcba3155d6c9c736db76
ashachakre0906 <chourpagar.asha@gmail.com>
2022-06-13 16:03:36 -0700

    added if condition to display colors for UV Index

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@
 var cityInput = document.getElementById("city-input");
 var searchCity = document.getElementById("submit");
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-// const fahrenheit = (temp * 9) / 5 + 32;
+// var fahrenheit = (temp * 9) / 5 + 32;
 searchCity.addEventListener("click",firstAPI);
 function firstAPI(event){
     event.preventDefault();
+    
     console.log(event);
     var cityName = cityInput.value.trim();
     if (event.target.innerText !== "Search"){
@@ -55,7 +56,7 @@ function secondAPI(lat,lon){
             // var header = document.getElementById("header-"+(i + 1))
             // header.innerText = data.daily[i].temp.max;
             var temp = document.getElementById("temp"+(i + 1))
-            temp.innerText = data.daily[i].temp.max;
+            temp.innerText = Math.floor(((data.daily[i].temp.max-273.15)*1.8)+32)+ "°F";
 
             var wind = document.getElementById("wind"+(i + 1))
             wind.innerText = data.daily[i].wind_speed;
@@ -65,18 +66,40 @@ function secondAPI(lat,lon){
 
             var uvIndex = document.getElementById("uv"+(i + 1))
             uvIndex.innerText = data.daily[i].uvi; 
+            if ( data.daily[i].uvi < 3) {
+                uvIndex.style.backgroundColor ="green"
+            }
+            else if (data.daily[i].uvi < 6) {
+      
+                uvIndex.style.backgroundColor ="yellow"
+            }
+            else if (data.daily[i].uvi < 8) {
+                uvIndex.style.backgroundColor ="orange"
+            }
+            else if (data.daily[i].uvi < 11) {
+                uvIndex.style.backgroundColor ="red"
 
+            }
+            else {
+                uvIndex.style.backgroundColor ="purple"
+
+            }
             var date = document.getElementById("date"+(i + 1))
             date.innerText = moment().tz(timeZone).add(i,"days").format("M/DD/YY")
             var iconUrl = `https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`;
 
             var icon = document.getElementById("icon"+(i + 1))
             icon.src = iconUrl;
+        }
+
+            
 
     }
-});
-     //2nd promise
+
+    );
 }
+
+     //2nd promise
 
 
 // getting previous searched city from the local storage and adding button 
@@ -92,9 +115,10 @@ function history(){
 }
 history();
 
-// function to save the city to local storage
-// submit.addEventListener("click".function()){
-//     firstAPI(cityName);
-//     searchHistory.push(cityName);
-//     localStorage.setItem("search",JSON.stringify("searchHistory"));
-
+var uvWarnings = {
+    green: "* You can safely stay outside using standard daily sun protection: broad spectrum SPF 30+ sunscreen containing zinc, sunglasses, and hat. Don't forget: in winter, reflection off snow can nearly double UV strength.",
+    yellow: "* Stay in the shade during late morning through mid-afternoon. Wear broad spectrum SPF 30+ sunscreen containing zinc, sunglasses, and hat.",
+    orange: "* Stay in the shade as much as possible, especially during late morning through mid-afternoon. Wear broad spectrum SPF 30+ sunscreen containing zinc, protective clothing (long-sleeved shirt and pants), sunglasses, and wide-brimmed hat.",
+    red: "* Extra protection needed. Be careful outside, especially during late morning through mid-afternoon. Stay in the shade as much as possible, especially during late morning through mid-afternoon. Wear broad spectrum SPF 30+ sunscreen containing zinc, protective clothing (long-sleeved shirt and pants), sunglasses, and wide-brimmed hat. Please note: white sand on the beach will reflect UV rays and can double UV exposure.",
+    purple: "* Extra protection needed. Avoid sun exposure during late morning through mid-afternoon. Unprotected skin and eyes can burn in minutes. Wear broad spectrum SPF 30+ sunscreen containing zinc, protective clothing (like long-sleeves), sunglasses, and wide-brimmed hat. Please note: white sand on the beach will reflect UV rays and can double UV exposure."
+}
